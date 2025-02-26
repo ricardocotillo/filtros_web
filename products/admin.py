9a653cdc3b64b717a25726d55c545f2508b3b5ba
ricardocotillo <ricardo.cotillo@gmail.com>
2025-02-25 20:11:21 -0500

    fix

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import Producto
+from .models import Producto, Marca, Modelo
 
-admin.site.register(Producto)
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'tipo',)
+    search_fields = ('codigo',)
+
+
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Marca)
+admin.site.register(Modelo)
