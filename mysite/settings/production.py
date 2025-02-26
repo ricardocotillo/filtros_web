5522396bbd1bf203ee0e4cba5fb2a96651f73448
ricardocotillo <ricardo.cotillo@gmail.com>
2025-02-25 19:15:04 -0500

    set environment

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,6 +1,25 @@
+import environ
 from .base import *
 
 DEBUG = False
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+SECRET_KEY = env('SECRET_KEY')
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
+}
 
 try:
     from .local import *
