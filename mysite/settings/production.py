09493ed73255a25679d83a0299ceff4d4ad53fc5
ricardocotillo <ricardo.cotillo@gmail.com>
2025-02-25 20:19:14 -0500

    fix settings

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -21,6 +21,13 @@ DATABASES = {
     }
 }
 
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = 'atencionalcliente@filtroswillybusch.com.pe'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+
 try:
     from .local import *
 except ImportError:
