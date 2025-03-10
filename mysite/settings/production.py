d252da8d16dfee55a29ab3fa6f5209c88f3aa906
ricardocotillo <ricardo.cotillo@gmail.com>
2025-03-10 16:26:36 -0500

    fix

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -25,7 +25,7 @@ EMAIL_HOST = env('EMAIL_HOST')
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'atencionalcliente@filtroswillybusch.com.pe'
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
 EMAIL_PORT = 465
 
 try:
